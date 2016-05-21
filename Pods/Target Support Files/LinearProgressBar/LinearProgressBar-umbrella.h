5620d1fa0646e2caf4da60e01de51fbda8109644
Jianxin Gao <chienshin1@gmail.com>
2016-05-21 01:54:18 -0700

    removed Pods directory from repo

--- Pods/Target Support Files/LinearProgressBar/LinearProgressBar-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double LinearProgressBarVersionNumber;
-FOUNDATION_EXPORT const unsigned char LinearProgressBarVersionString[];
-
--- Pods/Target Support Files/Pods-Restaurant Finder/Pods-Restaurant Finder-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Restaurant_FinderVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Restaurant_FinderVersionString[];
-
